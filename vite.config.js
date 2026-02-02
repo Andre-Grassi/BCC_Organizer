a185d1fc0166afbc3e4dac08e471ae9a15397cd1
andre-grassi <andregrassi.j@gmail.com>
2026-02-02 03:49:08 -0400

    build: add deploy configs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: '/BCC_Organizer/', // Nome do seu repositório GitHub
 })
